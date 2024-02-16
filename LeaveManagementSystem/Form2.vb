9d3717285338c36fab1ec095ec7318616479cbfb
sai santhosh bussa <saisanthoshbussa7@gmail.com>
2024-02-16 18:44:55 +0530

    commit 2

--- LeaveManagementSystem/Form2.vb
+++ LeaveManagementSystem/Form2.vb
@@ -1,7 +1,22 @@
-﻿Public Class Form2
+﻿Imports MySql.Data.MySqlClient
 
-    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        
+Public Class Form2
+    Dim connectionString As String = "server=127.0.0.1;uid=root;password=;database=leavemanagement"
+    Dim connection As New MySqlConnection(connectionString)
 
+    Private Sub ConnectToMySQL()
+        Try
+            connection.Open()
+            MessageBox.Show("Connected to MySQL Server")
+            ' You're connected, you can perform database operations here
+        Catch ex As Exception
+            MessageBox.Show("Error connecting to MySQL Server: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        ConnectToMySQL()
     End Sub
 End Class
