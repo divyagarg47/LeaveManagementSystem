4009751b4823f96c001b6b8356726fbc1aa0bd36
DevDev <devikas.iitg@gmail.com>
2024-02-16 16:10:00 +0530

    Not so full screen

--- LeaveManagementSystem/Form1.vb
+++ LeaveManagementSystem/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
-        Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        'Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
         Me.WindowState = FormWindowState.Maximized
     End Sub
 End Class
