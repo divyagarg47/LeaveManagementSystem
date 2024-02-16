07d63989a047d517c2eec197fbbb2cdaf8d592dc
sai santhosh bussa <saisanthoshbussa7@gmail.com>
2024-02-16 18:00:43 +0530

    commit 1

--- LeaveManagementSystem/Form1.vb
+++ LeaveManagementSystem/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
-    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
         Me.WindowState = FormWindowState.Maximized
 
@@ -8,7 +8,11 @@
         Panel_Login.Location = New Point((Me.Size.Width - Panel_Login.Size.Width) / 2, (Me.Size.Height - Panel_Login.Size.Height) / 2)
     End Sub
 
-    Private Sub Panel_Login_Paint(sender As System.Object, e As System.Windows.Forms.PaintEventArgs) Handles Panel_Login.Paint
+    Private Sub Panel_Login_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel_Login.Paint
+
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
 
     End Sub
 End Class
