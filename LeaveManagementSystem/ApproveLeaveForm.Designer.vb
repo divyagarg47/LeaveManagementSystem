bf5e3594860a8ed078e58b02c69f2b0c6771e28e
sai santhosh bussa <saisanthoshbussa7@gmail.com>
2024-02-17 17:43:14 +0530

    approve leave page updated

--- LeaveManagementSystem/ApproveLeaveForm.Designer.vb
+++ LeaveManagementSystem/ApproveLeaveForm.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form2
+Partial Class ApproveLeaveForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,28 +22,16 @@ Partial Class Form2
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(70, 92)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Form2
+        'ApproveLeaveForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(282, 253)
-        Me.Controls.Add(Me.Button1)
-        Me.Name = "Form2"
-        Me.Text = "Form2"
+        Me.Name = "ApproveLeaveForm"
+        Me.Text = "ApproveLeaveForm"
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
