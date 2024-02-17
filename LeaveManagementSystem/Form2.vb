b82daad32b79d934f0ba3f19a1b5839e0ae26632
sai santhosh bussa <saisanthoshbussa7@gmail.com>
2024-02-17 14:46:44 +0530

    commit 3

--- LeaveManagementSystem/Form2.vb
+++ LeaveManagementSystem/Form2.vb
@@ -8,6 +8,7 @@ Public Class Form2
         Try
             connection.Open()
             MessageBox.Show("Connected to MySQL Server")
+
             ' You're connected, you can perform database operations here
         Catch ex As Exception
             MessageBox.Show("Error connecting to MySQL Server: " & ex.Message)
@@ -15,8 +16,31 @@ Public Class Form2
             connection.Close()
         End Try
     End Sub
+    Private Sub InsertRecord(ByVal rollNumber As Integer, ByVal iitgId As String, ByVal password As String, ByVal name As String, ByVal department As String, ByVal programme As String, ByVal onLeave As Boolean)
+        Dim query As String = "INSERT INTO student (Roll_Number, IITG_ID, Password, Name, Department, Programme, On_Leave) VALUES (@rollNumber, @iitgId, @password, @name, @department, @programme, @onLeave)"
+
+        Try
+            connection.Open()
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@rollNumber", rollNumber)
+            command.Parameters.AddWithValue("@iitgId", iitgId)
+            command.Parameters.AddWithValue("@password", password)
+            command.Parameters.AddWithValue("@name", name)
+            command.Parameters.AddWithValue("@department", department)
+            command.Parameters.AddWithValue("@programme", programme)
+            command.Parameters.AddWithValue("@onLeave", onLeave)
+            command.ExecuteNonQuery()
+            MessageBox.Show("Record inserted successfully.")
+        Catch ex As Exception
+            MessageBox.Show("Error inserting record: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
 
     Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
-        ConnectToMySQL()
+        InsertRecord(210101033, "s.bussa", "wer", "Santhosh", "CSE", "B.Tech", False)
     End Sub
+
 End Class
--- LeaveManagementSystem/My Project/Application.Designer.vb
+++ LeaveManagementSystem/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.LeaveManagementSystem.Form1
+            Me.MainForm = Global.LeaveManagementSystem.Form2
         End Sub
     End Class
 End Namespace
