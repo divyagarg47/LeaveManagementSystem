8f7365b24ab96b2971cef002ac029e00615fe659
Divya <divyagarg4747@gmail.com>
2024-03-03 23:17:37 +0530

    commenting in home,login,change password pages

--- LeaveManagementSystem/LoginForm.vb
+++ LeaveManagementSystem/LoginForm.vb
@@ -5,10 +5,13 @@ Public Class LoginForm
     ' Connection string for your MySQL database
     Dim connectionString As String = "server=172.16.114.188;uid=santhosh;database=leavemanagement;"
 
+    ' Event handler for the Login button click
     Private Sub btnLogin_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnLogin.Click
+        ' Get user input from the textboxes
         Dim userId As String = txtUserID.Text
         Dim password As String = txtPassword.Text
 
+        ' Check if both fields are filled
         If String.IsNullOrEmpty(userId) OrElse String.IsNullOrEmpty(password) Then
             MessageBox.Show("Please enter both email and password.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             ' Exit the event, preventing further code execution
@@ -21,17 +24,22 @@ Public Class LoginForm
         If designation IsNot Nothing Then
             ' User exists in lookup table, now check the password in the corresponding table
             If CheckPassword(userId, password, designation) Then
+                ' Set global variables for user details
                 GlobalVariables.Email = userId
                 If designation = "hod" OrElse designation = "dean" OrElse designation = "director" Then
-                    ' If the value of designation is "HOD", change it to "Faculty"
                     designation = "faculty"
+                    GlobalVariables.Approver = "1"
                 ElseIf designation = "supervisor" OrElse designation = "head_supervisor" Then
-                    ' If the value of designation is "HOD", change it to "Faculty"
                     designation = "staff"
+                    GlobalVariables.Approver = "1"
                 End If
                 GlobalVariables.Designation = designation
+
+                ' Clear textboxes
                 txtUserID.Text = ""
                 txtPassword.Text = ""
+
+                ' Hide the login form and show the appropriate form based on designation
                 Me.Hide()
                 Dim homePageForm As New HomePageForm()
                 If designation = "admin" Then
@@ -66,12 +74,12 @@ Public Class LoginForm
         Using connection As New MySqlConnection(connectionString)
             connection.Open()
             GlobalVariables.Approver = "0"
+
+            ' if the user is an approver set him as approver
             If designation = "hod" OrElse designation = "dean" OrElse designation = "director" Then
-                ' If the value of designation is "HOD", change it to "Faculty"
                 designation = "faculty"
                 GlobalVariables.Approver = "1"
             ElseIf designation = "supervisor" OrElse designation = "head_supervisor" Then
-                ' If the value of designation is "HOD", change it to "Faculty"
                 designation = "staff"
                 GlobalVariables.Approver = "1"
             End If
@@ -80,19 +88,21 @@ Public Class LoginForm
             Using command As New MySqlCommand(query, connection)
                 command.Parameters.AddWithValue("@UserID", userId)
                 Dim storedPassword As String = Convert.ToString(command.ExecuteScalar())
+                ' match password with the stored password
                 Return password = storedPassword
             End Using
         End Using
     End Function
 
+    ' Event handler for the Login form load event
     Private Sub LoginForm_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'Me.FormBorderStyle = Windows.Forms.FormBorderStyle.None
+        ' Set the form state to maximized
         Me.WindowState = FormWindowState.Maximized
 
-        'Center the login page
+        ' Center the login page
         GroupBox1.Location = New Point((Me.Size.Width - GroupBox1.Size.Width) / 2, (Me.Size.Height - GroupBox1.Size.Height) / 2)
+
         ' Add a PictureBox for the IITG logo
-         ' Add a PictureBox for the IITG logo
         Dim iitgLogoPictureBox As New PictureBox()
         Try
             ' Load the image file from the same directory as the application executable
@@ -100,10 +110,10 @@ Public Class LoginForm
 
             ' Set PictureBox properties
             iitgLogoPictureBox.SizeMode = PictureBoxSizeMode.StretchImage
-            iitgLogoPictureBox.Size = New Size(100, 100) ' Set the size of the logo PictureBox
-            iitgLogoPictureBox.Location = New Point(GroupBox1.Location.X + ((GroupBox1.Size.Width - iitgLogoPictureBox.Size.Width) / 2), GroupBox1.Location.Y + 20) ' Position the logo at the top left corner
+            iitgLogoPictureBox.Size = New Size(100, 100)
+            iitgLogoPictureBox.Location = New Point(GroupBox1.Location.X + ((GroupBox1.Size.Width - iitgLogoPictureBox.Size.Width) / 2), GroupBox1.Location.Y + 20)
 
-
+            ' Add the PictureBox to the form
             Me.Controls.Add(iitgLogoPictureBox)
             iitgLogoPictureBox.BringToFront()
         Catch ex As Exception
@@ -112,9 +122,13 @@ Public Class LoginForm
         End Try
     End Sub
 
+    ' Event handler for the Change Password button click
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ' Clear textboxes
         txtUserID.Text = ""
         txtPassword.Text = ""
+
+        ' Hide the login form and show the Change Password form
         Me.Hide()
         ChangePasswordForm.Show()
     End Sub
